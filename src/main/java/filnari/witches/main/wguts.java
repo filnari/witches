ff05193d02d653ec989e0493c0a1a7298f32d284
filnari <filnari@gmail.com>
2015-09-29 16:57:26 -0700

    Reworking randomization of books.

--- src/main/java/filnari/witches/main/wguts.java
+++ src/main/java/filnari/witches/main/wguts.java
@@ -7,6 +7,7 @@ import org.bukkit.entity.LivingEntity;
 
 import java.io.File;
 import java.util.List;
+import java.util.Random;
 
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
@@ -20,7 +21,10 @@ import org.bukkit.inventory.meta.BookMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class wguts extends JavaPlugin implements Listener {
+	
 	public List<String> books;
+	public int chance;
+	
 	@Override
 	// This is run when the server starts
 	public void onEnable() {
@@ -40,10 +44,13 @@ public class wguts extends JavaPlugin implements Listener {
 			getLogger().info("config.yml found - loading");
 			// load config here
 		}
+		// Read the books from config.yml
 		books = getConfig().getStringList("books");
 		getLogger().info("Loaded "+books.size()+" books");
-		//for (int i = 0; i < books.size(); i++)
-		//	getLogger().info("Loaded book: " + books.get(i));
+		
+		// get the drop chance for books from config.yml
+		chance = getConfig().getInt("dropchance");
+		getLogger().info("Drop chance read: " + chance +"%");
 	}
 	@Override
 	public void onDisable() {
@@ -57,10 +64,14 @@ public class wguts extends JavaPlugin implements Listener {
 	{
 	LivingEntity entity = event.getEntity();
 	Player player = entity.getKiller();
-	// String[] books = { "Book1", "Book2", "Book3" };
+	
+	Random rn = new Random();
 
-	if (entity instanceof Witch && player != null) {
-		player.sendMessage("You killed a witch");
+	double xx = (double) chance / (double) 100;
+	double yy = Math.random();
+	getLogger().info("testing " + yy + " < " + xx);
+	if (entity instanceof Witch && player != null && yy < xx ) {
+		// player.sendMessage("You killed a witch");
 		
 		// Make the meta data object
 		ItemFactory factory = Bukkit.getItemFactory();
@@ -68,72 +79,33 @@ public class wguts extends JavaPlugin implements Listener {
 		meta.setAuthor("Witch Hazel");
 		meta.setTitle("Gate Secrets");
 		
-		// build the pages
+		// build the first page
 		StringBuilder page1 = new StringBuilder();
 		String p1a = "The secret to quick travel around the world is §9/gate§0.\n\n";
 		String p1b = "But, I'd be careful, old gates may be buried or hidden...\n\n";
 		String p1c = "Some say that there are many gates around the world, but one we do know about is...";
 		page1.append(p1a).append(p1b).append(p1c);
+
+		// Choose a random second page
 		StringBuilder page2 = new StringBuilder();
-		// String p2a = "Page 2, test 1\n";
-		// String p2b = "Page 2, test 2\n";
-		// page2.append(p2a).append(p2b);
-		
-		int random = (int) (Math.random() * 10 + 1);
-		// getLogger().info("random is " + random);
+		int random = (int) (Math.random() * books.size());
+		getLogger().info("Dropping book #" + random);
 		page2.append(books.get(random));
 		
-/*		switch (random) {
-		// color code §1 §0
-			case 1:
-				page2.append("Far to the southeast, there lies a wild unexplored peninsula, known to some as §aMonton§0.");
-				break;
-			case 2:
-				page2.append("To the northwest is a lake, this gate hides below it's pristine surface. It is §aUmel§0.");
-				break;
-			case 3:
-				page2.append("Hidden deep in a snowy forest in the north is §aNiveus§0.");
-				break;
-			case 4:
-				page2.append("§aHoogte§0 is where the tree canopy is so thick not even grass will grow on the ground.");
-				break;
-			case 5:
-				page2.append("Wedged in a tight canyon not far from town, you will find §aRoko§0.");
-				break;
-			case 6:
-				page2.append("On the coast of the sunny plains to the southeast, you will find §aFungo§0.");
-				break;
-			case 7:
-				page2.append("Recently found in a deep underground chasm, is §aAbismo§0.");
-				break;
-			case 8:
-				page2.append("§aSelva§0 is a well known gate on the edge of a deep, dark jungle.");
-				break;
-			case 9:
-				page2.append("In the plains of a wide savannah you can find §aUrbo§0.");
-				break;
-			case 10:
-				page2.append("Found on a forested isle in the far northeast, you will find §aLafo§0.");
-				break;
-			default:
-				page2.append("Nothing to see here. Move along.");
-				break;
-		} */
-		
 		// convert page text to meta data
-		// meta.setPages(page1.toString());
 		meta.setPages(page1.toString(),page2.toString());
 		
 		// create the book item
 		ItemStack item = new ItemStack(Material.WRITTEN_BOOK);
+		
 		// apply the page meta data to the book
 		item.setItemMeta(meta);
-		// and give it to the player
-		// player.getInventory().addItem(item);
-		// or drop it
+		
+		// To give it to the player: player.getInventory().addItem(item);
+		// we just drop it on the ground
 		World world = event.getEntity().getWorld();
 		world.dropItem(entity.getLocation(),item);
-		getLogger().info("Dropped a book #" + random);
+
 				
 		}
 	}
